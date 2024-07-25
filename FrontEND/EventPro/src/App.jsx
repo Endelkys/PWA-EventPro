d347c734e3f8188b96dbcd1aed83038b0ea1a66e
Endelkys <endelkysanelmar2604@gmail.com>
2024-07-25 19:54:32 -0400

    Recursos para el proyecto y modificanco el archivo App.jsx

---
+++ FrontEND/EventPro/src/App.jsx
@@ -0,0 +1,46 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./Views/Register";
+import Login from "./Views/Login";
+import Dashboard from "./Views/Dashboard";
+import PromotorPage from "./Views/PromotorPage";
+import LandingPage from "./Views/LandingPage";
+import AdminUsers from "./Views/AdminUsers";
+import Profile from "./Views/Profile";
+import AboutUs from "./Views/AboutUs";
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoutes from "./ProtectedRoutes";
+import Eventos from "./Views/Eventos";
+import Boletos from "./Views/Boletos";
+import Solicitud from "./Views/Solicitud";
+import Pagos from "./Views/Pagos";
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Rutas publicas */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/aboutUs" element={<AboutUs />} />
+
+          {/* Rutas protegidas */}
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/aboutUs" element={<AboutUs />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/promotor" element={<PromotorPage />} />
+            <Route path="/adminUsers" element={<AdminUsers />} />
+            <Route path="/eventos" element={<Eventos />} />
+            <Route path="/boletos" element={<Boletos />} />
+            <Route path="/solicitud" element={<Solicitud />} />
+            <Route path="/pagos" element={<Pagos />} />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
