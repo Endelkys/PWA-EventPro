f3583e42a1f5703ddd66283204b26afb8e7070d3
Daniel Rendon <rendondaniel2004@gmail.com>
2024-07-25 23:27:14 -0400

    Componentes Animaciones

---
+++ FrontEND/EventPro/src/components/Animations/Loading.jsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+
+  return (
+    <div className="flex justify-center items-center mt-40 mb-40">
+      <div className="loader ease-linear rounded-full border-8 border-t-8 dark:border-secondary-200 border-secondary-600 h-24 w-24 shadow-2xl dark:shadow-[#CAA8F5] shadow-[#a25bff] animate-spin"></div>
+    </div>
+  )
+}
+
+export default Loading
